7172c56e6b0afddd996b026cf7f1287b26e73830
nick <nikole.phipps12@gmail.com>
2024-02-04 01:14:52 -0800

    bugs fixed

--- App.py
+++ App.py
@@ -1,38 +1,71 @@
 import random
 import KnightRole
 import WizardRole
-import Game
 
+
+
+def role():
+    while True:
+
+        global roleasn
+        roleasn = (input('\nWill you be the “Brave Dashing Knight” or the “Powerful Wizard”? (Knight/Wizard) '))
+        
+        if (roleasn==('Knight')) or (roleasn==('knight')):
+            print('Oh I see you choosen the Knight wise choice!')
+            print('Your stats are:')
+            print(f'\nDefense: {KnightRole.defence}\nPower: {KnightRole.power}\nSpeed: {KnightRole.speed}')
+            print('\nall of your stats are measured on a scale from 0 to 10')
+            break
+
+
+        elif (roleasn==('Wizard')) or (roleasn==('wizard')):
+            print('Oh I see you choosen the Wizard wise choice!')
+            print('Your stats are:')
+            print(f'\nDefense: {WizardRole.defence}\nPower:{WizardRole.power}\nSpeed: {WizardRole.speed}')
+            print('\nall of your stats are measured on a scale from 0 to 10')
+            break
+
+        else:
+            print('\nSorry traveller that is not one of the roles you must pick today')
+
+    #Used to get the players name
+    global name
+    name = str(input('\nNow that your role is decided, what will I call you through this quest? '))
+    
+
+    print(f'\n{name} what a fine name indeed traveler!')
+    print('\nNow that all formalities are out of the way it is time for your first quest')
+    print('\nIn total there are 3 quests to reach The Lost Treasure and they all depend on your luck')
 
 def darkForest():
     d1 = random.randint(1, 10)
     d2 = random.randint(1, 10)
  
-    if (Game.roleasn==('Knight')) or (Game.roleasn==('knight')):
+    if (roleasn==('Knight')) or (roleasn==('knight')):
 
         dDefence = KnightRole.defence + d1 + d2
 
         if dDefence >= 15:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nWhile if was tough with all of your armour you were able to outrun the witch! Congrats {Game.name}')
+            print(f'\nWhile if was tough with all of your armour you were able to outrun the witch! Congrats {name}')
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\n You tripped over all of your gear and were not able to outrun the witch better luck next time {Game.name}')
+            print(f'\n You tripped over all of your gear and were not able to outrun the witch better luck next time {name}')
             print('\nGAME OVER')
             exit()
             
-    if (Game.roleasn==('Wizard')) or (Game.roleasn==('wizard')):
+    if (roleasn==('Wizard')) or (roleasn==('wizard')):
 
         dDefence = WizardRole.defence + d1 + d2
 
         if dDefence >= 15:   
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nWhile it was not easy you were able to speed through the forest on your broom and outrun the witch! Congrats {Game.name}')
+            print(f'\nWhile it was not easy you were able to speed through the forest on your broom and outrun the witch! Congrats {name}')
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nWhile looking back you flew into a tree and were not able to outrun the witch, better luck next time {Game.name}.')
+            print(f'\nWhile looking back you flew into a tree and were not able to outrun the witch, better luck next time {name}.')
             print('\nGAME OVER')
             exit()
 
@@ -41,7 +74,7 @@ def dungeonTrap():
     d1 = random.randint(1, 10)
     d2 = random.randint(1, 10)
  
-    if (Game.roleasn==('Knight')) or (Game.roleasn==('knight')):
+    if (roleasn==('Knight')) or (roleasn==('knight')):
         
         dSpeed = KnightRole.speed + d1 + d2
 
@@ -51,11 +84,11 @@ def dungeonTrap():
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nThough you were quick there was one trap you were not able to see, with such a wound there is no way youll make it, better luck next time {Game.name}')
+            print(f'\nThough you were quick there was one trap you were not able to see, with such a wound there is no way youll make it, better luck next time {name}')
             print('\nGAME OVER')
             exit()
             
-    if (Game.roleasn==('Wizard')) or (Game.roleasn==('wizard')):
+    if (roleasn==('Wizard')) or (roleasn==('wizard')):
 
         dSpeed = WizardRole.speed + d1 + d2
 
@@ -65,7 +98,7 @@ def dungeonTrap():
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nWhile flying through the dungeon you were caught off guard by a floor trap, there is no ecaping from this \nbetter luck nect time {Game.name}')
+            print(f'\nWhile flying through the dungeon you were caught off guard by a floor trap, there is no ecaping from this \nbetter luck nect time {name}')
             print('\nGAME OVER')
             exit()
 
@@ -74,37 +107,36 @@ def dragonFight():
     d1 = random.randint(1, 10)
     d2 = random.randint(1, 10)
  
-    if (Game.roleasn==('Knight')) or (Game.roleasn==('knight')):
+    if (roleasn==('Knight')) or (roleasn==('knight')):
 
-        dSpeed = KnightRole.power + KnightRole.defence + d1 + d2
+        dPower = KnightRole.power + KnightRole.defence + d1 + d2
 
-        if dSpeed >= 30:
+        if dPower >= 30:
             print(f'\nYour dice landed on {d1} and {d2}')
             print(f'\nThough it was tough with your armour and sword you were able to slay the dragon')
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nYou were not strong enough to defeat the dragon and with these injuries there is going back, better luck next time {Game.name}')
+            print(f'\nYou were not strong enough to defeat the dragon and with these injuries there is going back, better luck next time {super.name}')
             print('\nGAME OVER')
             exit()
             
-    if (Game.roleasn==('Wizard')) or (Game.roleasn==('wizard')):
+    if (roleasn==('Wizard')) or (roleasn==('wizard')):
 
-        dSpeed = WizardRole.power + WizardRole.defence + d1 + d2
+        dPower = WizardRole.power + WizardRole.defence + d1 + d2
 
-        if dSpeed >= 30:   
+        if dPower >= 30:   
             print(f'\nYour dice landed on {d1} and {d2}')
             print(f'\nWith your magic abilities you were able to swiftly slay the dragon')
 
         else:
             print(f'\nYour dice landed on {d1} and {d2}')
-            print(f'\nEven with your great power one hit from the dragon toom you out due to your low defence, better luck next time {Game.name}')
+            print(f'\nEven with your great power one hit from the dragon toom you out due to your low defence, better luck next time {name}')
             print('\nGAME OVER')
             exit()
                        
-print(f'\nThe Lost treasure is now all yours {Game.name} Congrats!')
-print('\nWINNER')
-exit()
+
+
     
 
 
--- Game.py
+++ Game.py
@@ -1,119 +1,91 @@
 #Dnd text game
-import KnightRole
-import WizardRole
 import App
 
 print('Hello fellow traveler, welcome To your quest to find The Lost Treasure!')
 print('\nWhat path will you choose to follow today?')
 
-while True:
-#loop to make player choose their role
-
-    roleasn = (input('\nWill you be the “Brave Dashing Knight” or the “Powerful Wizard”? (Knight/Wizard)'))
-
-    if (roleasn==('Knight')) or (roleasn==('knight')):
-        print('Oh I see you choosen the Knight wise choice!')
-        print('Your stats are:')
-        print(f'\nDefense: {KnightRole.defence}\nPower: {KnightRole.power}\nSpeed: {KnightRole.speed}')
-        print('\nall of your stats are measured on a scale from 0 to 10')
-        break
-
-
-    elif (roleasn==('Wizard')) or (roleasn==('wizard')):
-        print('Oh I see you choosen the Wizard wise choice!')
-        print('Your stats are:')
-        print(f'\nDefense: {WizardRole.defence}\nPower:{WizardRole.power}\nSpeed: {WizardRole.speed}')
-        print('\nall of your stats are measured on a scale from 0 to 10')
-        break
-
-    else:
-        print('\nSorry traveller that is not one of the roles you must pick today so,')
-
-#Used to get the players name
-name = input('\nNow that your role is decided, what will I call you through this quest? ')
-
-print(f'\n{name} what a fine name indeed traveler!')
-print('\nNow that all formalities are out of the way it is time for your first quest')
-print('\nIn total there are 3 quests to reach The Lost Treasure and they all depend on your luck')
-
+print(App.role())
 
 while True:    
 
-    question = (input(f'\nSo {name} are you ready for your first quest? '))
+    question = (input('\nSo are you ready for your first quest? (yes/no) '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
 
     else:
-        print(f'You must say yes to continue {name}')
+        print(f'You must say yes to continue')
 
 
 #challenge one   
 print('\nFor your first quest you must run through the dark forest but there lies an evil witch, beware for if you are caught there is no turning back')
 while True:    
 
-    question2 = (input(f'\nSo {name} would you like to roll your dice? \n*You can role 2 dice, each one with 10 sides 1-10*'))
+    question2 = (input('\nSo would you like to roll your dice? (yes/no) \n\n*You can role 2 dice, each one with 10 sides 1-10* '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
 
     else:
-        print(f'\nYou must roll the dice to continue{name}')
+        print(f'\nYou must roll the dice to continue')
 
-App.darkForest()
+print(App.darkForest())
 
 #challenge two
 while True:    
 
-    question = (input(f'\n{name} Your next quest will be more challenging are you ready for the challenge? '))
+    question = (input('\n Your next quest will be more challenging are you ready for the challenge? '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
 
     else:
-        print(f'You must say yes to continue {name}')
+        print('You must say yes to continue')
 
     
 print('\nFor this quest you must make your way through the treasures dungeon you must be quick and ovoid all the traps')
 while True:    
 
-    question2 = (input(f'\nSo {name} would you like to roll your dice? '))
+    question2 = (input('\nSo would you like to roll your dice? '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
 
     else:
-        print(f'\nYou must roll the dice to continue{name}')
+        print('\nYou must roll the dice to continue')
 
-App.dungeonTrap()
+print(App.dungeonTrap())
 
 #challenge three
 while True:    
 
-    question = (input(f'Your are strong indeed {name} no one else has made it this far, are you ready for your last quest? '))
+    question = (input('Your are strong indeed no one else has made it this far, are you ready for your last quest? '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
 
     else:
-        print(f'\nYou must say yes to continue {name}')
+        print(f'\nYou must say yes to continue')
 
     
 print('\nFor this quest your strength and defence will be crucial \nFor this quest you will have to slay the dragon protecting The Lost Treasure')
 while True:    
 
-    question2 = (input(f'\nSo {name} would you like to take on this challenge and roll your dice? '))
+    question2 = (input(f'\nSo would you like to take on this challenge and roll your dice? '))
 
     if (question == ('Yes')) or (question == ('yes')):
         break
     elif (question == ('give up')):
-        print(f'It was a good try {name} better luck next time.')
+        print('It was a good try better luck next time.')
         exit()
     else:
-        print(f'\nYou must roll the dice to continue or give up {name}')
+        print(f'\nYou must roll the dice to continue or give up')
 
 App.dragonFight()
-    
+
+print(f'\nThe Lost treasure is now all yours Congrats!')
+print('\nWINNER')
+exit()
 
 
 
